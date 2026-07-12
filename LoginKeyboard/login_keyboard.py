7d61917ea5c25b389bb1e84a185fae175b9f5a78
신정환 <loginshin3@gmail.com>
2026-07-12 12:24:17 +0900

    🔨 chore(loginkeyboard) : 파일 이동 및 제거

--- LoginKeyboard/login_keyboard.py
+++
@@ -1,7 +0,0 @@
-"""PyInstaller entry point for the Windows LoGinKeyboard app."""
-
-from login_keyboard_windows.app import run_with_error_dialog
-
-
-if __name__ == "__main__":
-    run_with_error_dialog()
